846260ecfbb5f2d02023c013b1e5dceb72492b4a
Ravikant Sharma <ravikantuk18@gmail.com>
2026-07-14 23:43:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import Posts from './components/Posts'
+
+function App() {
+
+  return (
+    <div>
+     <Posts />
+    </div>
+  )
+}
+
+export default App
